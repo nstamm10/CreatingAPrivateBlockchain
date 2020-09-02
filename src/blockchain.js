d546ba7c2d5e94f3a74f1e1eacbf24e033dc9140
bennystamm <40069838+bennystamm@users.noreply.github.com>
2020-09-02 14:35:20 -0400

    deleted ) and semicolon

--- src/blockchain.js
+++ src/blockchain.js
@@ -98,7 +98,7 @@ class Blockchain {
      */
     requestMessageOwnershipVerification(address) {
         return new Promise((resolve) => {
-          resolve(address + ":" + new Date().getTime().toString().slice(0,-3); + ":starRegistry"));
+          resolve(address + ":" + new Date().getTime().toString().slice(0,-3); + ":starRegistry");
         });
     }
 
